5cadf6e9dea7544800fc29fc42b61394ffb40663
Oniely <jabagatniel09@gmail.com>
2025-07-01 00:40:43 +0800

    profile information form, and change password form for admin profile

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-4rem)]">
+    <div className="flex-1 flex items-center justify-center">
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
     </div>
   );
