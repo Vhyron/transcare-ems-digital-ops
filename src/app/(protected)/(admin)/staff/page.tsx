37c195a2cd0955477ae440ec8166d45293be5c80
Oniely <jabagatniel09@gmail.com>
2025-07-02 09:47:52 +0800

    changes:adjusted some h1 sizes for consistency

--- src/app/(protected)/(admin)/staff/page.tsx
+++ src/app/(protected)/(admin)/staff/page.tsx
@@ -19,9 +19,8 @@ const StaffPage = () => {
   return (
     <>
       <div className="mb-4">
-        <h1 className="text-2xl font-bold">All Staff</h1>
-
-        <p className="text-muted-foreground mt-1">
+        <h1 className="text-3xl font-bold tracking-tight">All Staff</h1>
+        <p className="text-muted-foreground">
           Fill out the form below to add a new staff member to the system. All
           required fields are marked with an asterisk (*).
         </p>
