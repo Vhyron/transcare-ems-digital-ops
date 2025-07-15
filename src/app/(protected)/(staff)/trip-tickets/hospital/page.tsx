ba172446f795235912471a26cf2182d31d0083c1
Rai M. Reyes Jr. <2r.reiyhes@gmail.com>
2025-07-15 12:14:48 +0800

    added signature component

--- src/app/(protected)/(staff)/trip-tickets/hospital/page.tsx
+++ src/app/(protected)/(staff)/trip-tickets/hospital/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
